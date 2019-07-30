5823ef65660102ae6cb0e76ee9053ee708bcc630
Sebastian Blanco <sblanco@devartis.com>
2019-07-30 13:00:03 -0300

    complete tests

--- spec/controllers/home_spec.rb
+++ spec/controllers/home_spec.rb
@@ -3,5 +3,6 @@ require_relative '../../app/controllers/home_controller'
 describe HomeController do
   it "returns true" do
     home = HomeController.new
+    expect(home).not_to be_nil
   end
 end
