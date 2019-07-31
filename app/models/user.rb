7eb797ce731d77de29ec32f0b003aec1fc953267
Sebastian Blanco <sblanco@devartis.com>
2019-07-31 15:28:03 -0300

    Adds the user create endpoint

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :trackable
 
   validates :user_name, presence: :true, uniqueness: { case_sensitive: true }
 
