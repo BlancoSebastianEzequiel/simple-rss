f3c1eb984606915de70d4032bd1150437a9fcb57
sebastian <sblanco@devartis.com>
2019-07-31 19:55:16 -0300

    Adds user authentication token

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :trackable
 
   validates :user_name, presence: :true, uniqueness: { case_sensitive: true }, format: { without: /\s/ }
+  validates :auth_token, uniqueness: true
+
+  before_create :generate_authentication_token!
+  def generate_authentication_token!
+    begin
+      self.auth_token = Devise.friendly_token
+    end while self.class.exists?(auth_token: auth_token)
+  end
 
   protected
   # From Devise module Validatable
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_31_141640) do
+ActiveRecord::Schema.define(version: 2019_07_31_221636) do
 
   create_table "users", force: :cascade do |t|
     t.string "user_name", default: "", null: false
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_07_31_141640) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "auth_token", default: ""
+    t.index ["auth_token"], name: "index_users_on_auth_token", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
