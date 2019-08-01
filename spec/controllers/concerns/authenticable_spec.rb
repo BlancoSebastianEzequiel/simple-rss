6bc732f014db808e8afb032622579bb74805c33f
sebastian <sblanco@devartis.com>
2019-07-31 22:37:54 -0300

    Adds the authenticate with token method to handle access to actions

---
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+class Authentication  < ActionController::Base
+  include Authenticable
+end
+
+RSpec.describe Authenticable do
+  let(:authentication) { Authentication.new }
+  subject { authentication }
+
+  describe "#current_user" do
+    before do
+      @user = FactoryBot.create :user
+      request.headers["Authorization"] = @user.auth_token
+      allow(authentication).to receive(:request).and_return(request)
+    end
+    it "returns the user from the authorization header" do
+      expect(authentication.current_user.auth_token).to eql @user.auth_token
+    end
+  end
+
+  describe "#authenticate_with_token" do
+    before do
+      @user = FactoryBot.create :user
+      allow(authentication).to receive(:current_user).and_return(nil)
+      allow(response).to receive(:response_code).and_return(401)
+      allow(response).to receive(:body).and_return({"errors" => "Not authenticated"}.to_json)
+      allow(authentication).to receive(:response).and_return(response)
+    end
+
+    it "render a json error message" do
+      expect(json_response[:errors]).to eql "Not authenticated"
+    end
+
+    it {  should respond_with 401 }
+  end
+end
