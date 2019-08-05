bfac8b012fa7d4138640196b4aa72139eb08f82d
Sebastian Blanco <sblanco@devartis.com>
2019-08-05 11:21:22 -0300

    uncomment migration join table fields.

--- db/migrate/20190803040734_create_join_table_users_feeds.rb
+++ db/migrate/20190803040734_create_join_table_users_feeds.rb
@@ -1,8 +1,8 @@
 class CreateJoinTableUsersFeeds < ActiveRecord::Migration[5.2]
   def change
     create_join_table :users, :feeds do |t|
-      # t.index [:user_id, :feed_id]
-      # t.index [:feed_id, :user_id]
+      t.index [:user_id, :feed_id]
+      t.index [:feed_id, :user_id]
     end
   end
 end
