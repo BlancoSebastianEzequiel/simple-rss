387717bcdc9436877acd56eea283fc2683c17ff2
sblanco <sblanco@devartis.com>
2019-08-07 10:17:08 -0300

    add commentary that explain a future refactor on the feed delete query

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -31,6 +31,10 @@ class FeedsController < ApplicationController
   end
 
   def destroy
+    # Esta query necesita ser refactorizada en otro branch ya que se necesita borrar la asociacion entre el articulo
+    # y el ususario. Y en caso de que el articulo no tenga mas ususarios, borrar el articulo. Y finalmente, borrar
+    # el feed si no tiene mas usuarios. La idea es refactorizar de manera de hacerlo en una sola query y agregar un
+    # delete on cascade para evitar borrar relaciones a mano.
     feed = current_user.feeds.find(params[:id])
     current_user.feeds.delete(feed)
     feed.users.delete(current_user)
