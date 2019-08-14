22576a20ff90d6bdbf7669786ded2f2fd1193e53
sebastian <sblanco@devartis.com>
2019-08-14 00:07:43 -0300

    mount the uploader to the articles model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,7 @@ class Article < ApplicationRecord
   belongs_to :feed
   has_many :articles_user
   has_many :users, :through => :articles_user
+  mount_uploader :avatar, AvatarUploader
 
   validates :link, presence: :true, uniqueness: { case_sensitive: true }
   validates_format_of :link, :with => URI::regexp
