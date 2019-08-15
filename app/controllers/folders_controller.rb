af1ffb673d5f3ec61695cfe0b45c92c6f3260a80
sblanco <sblanco@devartis.com>
2019-08-15 15:50:56 -0300

    Add create folder implementation

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -4,6 +4,17 @@ class FoldersController < ApplicationController
   before_action :authenticate_with_token!, :only => [:create]
 
   def create
-    render json: { name: "" }, status: :created
+    folder = Folder.new(folder_params)
+    if folder.save
+      render json: folder, status: :created
+    else
+      render json: { errors: folder.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def folder_params
+    params.require(:folder).permit(:name)
   end
 end
