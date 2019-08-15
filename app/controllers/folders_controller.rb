6faf5d7889401570220c9f8d5033480b37e789c1
sblanco <sblanco@devartis.com>
2019-08-15 15:46:10 -0300

    add simple implementation that fails

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,2 +1,9 @@
 class FoldersController < ApplicationController
+  respond_to :json
+  wrap_parameters :feed, include: %i[url]
+  before_action :authenticate_with_token!, :only => [:create]
+
+  def create
+    render json: { name: "" }, status: :created
+  end
 end
