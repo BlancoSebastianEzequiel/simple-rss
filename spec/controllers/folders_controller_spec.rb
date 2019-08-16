ea07a00ba19dcc12c391a794d3d401fd85a7c024
sebastian <sblanco@devartis.com>
2019-08-15 22:49:41 -0300

    Refactor test by passing a list of feed ids to store on a folder by the current user

--- spec/controllers/folders_controller_spec.rb
+++ spec/controllers/folders_controller_spec.rb
@@ -4,17 +4,20 @@ RSpec.describe FoldersController, type: :controller do
   before(:each) do
     @user = FactoryBot.create :user
     api_authorization_header(@user.auth_token)
+    @feed = FactoryBot.create(:feed)
   end
+
   describe "POST #create" do
     context "when is successfully created" do
       before(:each) do
-        @folder_attributes = FactoryBot.attributes_for :folder
-        post :create, params: { folder: @folder_attributes }, format: :json
+        @name = FactoryBot.attributes_for(:folder)[:name]
+        post :create, params: { folder: { name: @name, feeds_id: [ @feed.id ] } }, format: :json
       end
 
       it "renders the json representation for the folder record just created" do
         feed_response = json_response
-        expect(feed_response[:name]).to eql @folder_attributes[:name]
+        expect(feed_response[:folder][:name]).to eql @name
+        expect(feed_response[:feeds_id][0]).to eql @feed.id
       end
     end
   end
