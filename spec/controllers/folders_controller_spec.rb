072a8ebf271b825742b788543ed15f4d50ada85d
sblanco <sblanco@devartis.com>
2019-08-16 09:00:28 -0300

    test getting folder wothout creating them

--- spec/controllers/folders_controller_spec.rb
+++ spec/controllers/folders_controller_spec.rb
@@ -34,15 +34,27 @@ RSpec.describe FoldersController, type: :controller do
   end
 
   describe "GET #show" do
-    before(:each) do
-      @name = FactoryBot.attributes_for(:folder)[:name]
-      post :create, params: { folder: { name: @name, feeds_id: [ @feed.id ] } }, format: :json
-      get :show, params: { feed_id: @feed.id }, format: :json
-    end
     context "when we get the folder after storing feeds on them" do
+      before(:each) do
+        @name = FactoryBot.attributes_for(:folder)[:name]
+        post :create, params: { folder: { name: @name, feeds_id: [ @feed.id ] } }, format: :json
+        get :show, params: { feed_id: @feed.id }, format: :json
+      end
       it "returns the list of folders" do
         feed_response = json_response
         expect(feed_response.length).to eql 1
+      end
+
+      it { should respond_with :ok }
+    end
+
+    context "when we get the folder without creating it" do
+      before(:each) do
+        get :show, params: { feed_id: @feed.id }, format: :json
+      end
+      it "returns an empty list of folders" do
+        feed_response = json_response
+        expect(feed_response.length).to eql 0
       end
 
       it { should respond_with :ok }
